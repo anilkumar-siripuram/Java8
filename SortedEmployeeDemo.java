d8407a918a65a1d83ecb41bf9979bd04751d3415
Anil Kumar Siripuram <64580868+anilkumar-siripuram@users.noreply.github.com>
2022-07-13 14:29:56 +0530

    Add files via upload
    Java 8 Streams and CompletableFuture.

---
+++ SortedEmployeeDemo.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class SortedEmployeeDemo {
+    public static void main(String[] args) {
+
+        List<Employee> empList = new ArrayList<>();
+        empList.add(new Employee("Nataraja G", "Accounts", 8000));
+        empList.add(new Employee("Nagesh Y", "Admin", 15000));
+        empList.add(new Employee("Vasu V", "Security", 2500));
+        empList.add(new Employee("Amar", "Entertainment", 12500));
+
+       empList.stream().sorted((e1,e2)->e1.getSalary().compareTo(e2.getSalary())).forEach(System.out ::println);
+
+       // HashSet
+
+    }
+}
